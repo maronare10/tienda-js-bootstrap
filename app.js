9e15a8b25fd13dda1713b448fb016cf347d8d589
Mariana Navarro <maronare10@gmail.com>
2023-01-22 02:53:04 -0500

    creando función listar categorias, botones y sus eventos

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 async function renderCategories() {
-  const response = await fetch ('https://rickandmortyapi.com/api/character')
+  const response = await fetch ('https://fakestoreapi.com/products/categories')
   const categories =  await response.json()
-  console.log(response)
-  console.log(categories)
-}
+  // console.log(response)
+  // console.log(categories)
+  
+  const categoriesSection = document.querySelector('.categories')
+  let categoryButtons = ''
+  
+  categories.forEach(category => {
+    categoryButtons = categoryButtons + `<button>${category}</button>`
+  })
+  // console.log(categoryButtons)
+  
+  categoriesSection.innerHTML = categoryButtons
 
+  const buttons = document.querySelectorAll('button')
+
+  buttons.forEach(button => {
+    button.addEventListener('click', event => {
+      console.log(event)
+      fetchProductsByCategory(event.target.textContent)
+    })
+
+  })
+}
 renderCategories()
+
+
