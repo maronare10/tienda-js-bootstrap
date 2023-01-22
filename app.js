007ad112e4c7cbfdf07976eef43da684c07fd17c
Mariana Navarro <maronare10@gmail.com>
2023-01-22 02:58:20 -0500

    renderizando listado de productos con imagen, descripción, título y precio

--- app.js
+++ app.js
@@ -27,3 +27,26 @@ async function renderCategories() {
 renderCategories()
 
 
+async function fetchProductsByCategory(categoryName) {
+  const response = await fetch(`https://fakestoreapi.com/products/category/${categoryName}`)
+  // console.log(response)
+  const products = await response.json()
+  // console.log(products)
+
+  const productsSection = document.querySelector('.products')
+  let productElements = ''
+
+  products.forEach(product => {
+    productElements = productElements + `
+    <article class="product">
+      <img class="product__image" src="${product.image}" alt="imagen de producto">
+      <div class="product__description">
+        <h2 class="product__title">${product.title}</h2>
+        <p class="product__price" >S/ ${product.price}</p>
+      </div>
+    </article>
+    `
+  })
+
+  productsSection.innerHTML = productElements
+}
