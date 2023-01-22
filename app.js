618569c277c65d1440031a8462f59311dc9d15ff
Mariana Navarro <maronare10@gmail.com>
2023-01-22 00:33:29 -0500

    haciendo el llamado a fakestoreapi

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('aquí funciona')
+async function renderCategories() {
+  const response = await fetch ('https://rickandmortyapi.com/api/character')
+  const categories =  await response.json()
+  console.log(response)
+  console.log(categories)
+}
+
+renderCategories()
